26d5cee7dd53515ec2771e62124ae07e6eb4a30c
Kevin Sirait <siraitkevin25@gmail.com>
2025-10-15 13:25:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.green,),
+        body: Center(
+          child: Text("Project PM",style: TextStyle(fontSize: 32),),
+        ),
+      ),
+    );
+  }
+}
