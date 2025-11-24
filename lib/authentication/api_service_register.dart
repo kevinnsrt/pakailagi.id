6ae2e9bd1402bee1b4c447057749c10a9d69e3db
Kevin Sirait <siraitkevin25@gmail.com>
2025-11-24 13:51:53 +0700

    update api endpoint

--- lib/authentication/api_service_register.dart
+++ lib/authentication/api_service_register.dart
@@ -10,7 +10,7 @@ class ApiServiceRegister {
     required String token,
   }) async {
 
-    final url = Uri.parse("http://10.168.39.176/api/firebase-register");
+    final url = Uri.parse("http://172.20.10.5/api/firebase-register");
 
     final response = await http.post(
       url,
